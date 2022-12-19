20302c90467b49b8badfdd32d55de5817fc5a263
sensiz <dev.federicosensi@gmail.com>
2022-12-19 16:33:40 +0100

    adding bonus

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,21 @@
 <header>
 
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <nav class="navbar navbar-expand-sm">
         <div class="container">
-            <a class="navbar-brand" href="#">Movies</a>
+            <a href="#">Movies</a>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" aria-current="page">Home<span class="visually-hidden">(current)</span></a>
+                        <a href="#" aria-current="page">Home<span class="visually-hidden">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                        <a href="#">Contact</a>
                     </li>
                 </ul>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center pt-5">Welcome</h1>
+<h1 class="text-center pt-5">The Movie Database!</h1>
 
 <div class="content">
     <div class="container my-5">
-        <div class="row row-cols-4 g-3">
+        <div class="row row-cols-5 g-3">
             @forelse($movies as $movie)
             <div class="col">
-                <div class="card">
-                    <div class="card-body">
+                <div class="my-card">
+                    <img src="{{$movie->cover}}" alt="">
+
+                    <div class="back-card">
                         <h3>{{$movie->title}}</h3>
                         <p>{{$movie->original_title}}</p>
                         <p class="text-muted">{{$movie->nationality}}</p>
                         <p>{{$movie->date}}</p>
-                        <h5>{{$movie->vote}}</h5>
+                    </div>
+
+                    <div class="circle-vote">
+                        <p class="m-0">
+                            {{$movie->vote}}
+                        </p>
                     </div>
                 </div>
             </div>
             @empty
-            <div class="col-12">
-                <p>
-                    🙄 Sorry, no books in the labrary yet.
-                </p>
-            </div>
+            <div class="col"></div>
             @endforelse
         </div>
     </div>
