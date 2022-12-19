4cdf0fb1a04c1650940769a77398196b5b60a222
sensiz <dev.federicosensi@gmail.com>
2022-12-19 15:41:52 +0100

    markup created &  db imported

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Lara-library @yield('title', 'Welcome')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    @include('partials.header')
+    <!-- Site header -->
+    <main>
+        @yield('content')
+    </main>
+    <!-- Site Main -->
+    @include('partials.footer')
+    <!-- Site Footer -->
+
+</body>
+
+</html>
